b34dfd4cbea55295eb082467861e3dcf15aa1d87
Kairnex <codexkairnex@gmail.com>
2025-02-19 01:56:14 +0530

    Update config.py

--- config.py
+++ config.py
@@ -8,19 +8,19 @@ if os.path.exists("local.env"):
 load_dotenv()
 class Config(object):
     log = True
-    APP_ID = getenv("API_ID", "6435225")
-    API_HASH = getenv("API_HASH", "4e984ea35f854762dcde906dce426c2d")
-    TOKEN = getenv("TOKEN", "")
-    OWNER_ID = getenv("OWNER_ID", "")
+    APP_ID = getenv("API_ID", "9696783")
+    API_HASH = getenv("API_HASH", "3e74a9830493e9261410a947428dbb34")
+    TOKEN = getenv("TOKEN", "8098975400:AAGSHD7BSnpUaZPljFJvkjt3RKRpRxMgTC0")
+    OWNER_ID = getenv("OWNER_ID", "7538572906")
     ASSISTANT_ID = getenv("ASSISTANT_ID", "")
     STRING_SESSION = getenv("STRING_SESSION", "") #telethon
-    OWNER_USERNAME = getenv("OWNER_USERNAME", "")
+    OWNER_USERNAME = getenv("OWNER_USERNAME", "@its_deva_heree")
     DB_URI = getenv("DATABASE_URL", "")
     DB_URI = DB_URI.replace("postgres", "postgresql")
-    MESSAGE_DUMP = getenv("MESSAGE_DUMP", "-1001509525202")
-    GBAN_LOGS = getenv("GBAN_LOGS", "-1001509525202")
-    SYS_ADMIN = getenv("SYS_ADMIN", "1669178360")
-    DEV_USERS = getenv("DEV_USERS", "1669178360")
+    MESSAGE_DUMP = getenv("MESSAGE_DUMP", "-1002404751523")
+    GBAN_LOGS = getenv("GBAN_LOGS", "-1002404751523")
+    SYS_ADMIN = getenv("SYS_ADMIN", "7225660023")
+    DEV_USERS = getenv("DEV_USERS", "7225660023")
     LOAD = getenv("LOAD")
     WEBHOOK = False
     SPB_MODE = True
@@ -37,8 +37,8 @@ class Config(object):
     CUSTOM_CMD = False
     CHANNEL = getenv("CHANNEL", "TheUpdatesChannel")
     SUPPORT = getenv("SUPPORT", "TheSupportChat")
-    START_IMG = os.environ.get("START_IMG", "https://telegra.ph/file/35a7b5d9f1f2605c9c0d3.png")
-    CMD_IMG = os.environ.get("CMD_IMG", "https://telegra.ph/file/66518ed54301654f0b126.png")
+    START_IMG = os.environ.get("START_IMG", "https://te.legra.ph/file/4a7c28726502e24ea0fe0.jpg")
+    CMD_IMG = os.environ.get("CMD_IMG", "https://te.legra.ph/file/4a7c28726502e24ea0fe0.jpg")
     CASH_API_KEY = getenv("CASH_API_KEY", "https://www.alphavantage.co/support/#api-key")
     TIME_API_KEY = getenv("TIME_API_KEY", "https://timezonedb.com/api")
     WALL_API = getenv("WALL_API", "https://wall.alphacoders.com/api.php")
@@ -48,6 +48,6 @@ class Config(object):
     CF_API_KEY = getenv("CF_API_KEY", "coffehouse.intellivoid.net")
     BOT_API_URL = getenv("BOT_API_URL", "https://api.telegram.org/bot")
     BOT_API_FILE_URL = getenv("BOT_API_FILE_URL", "https://api.telegram.org/file/bot")
-    SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1669178360").split()))
-    ZAID_USER = list(map(int, getenv("DEV_USERS", "1669178360").split()))
+    SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7225660023").split()))
+    ZAID_USER = list(map(int, getenv("DEV_USERS", "7225660023").split()))
     NO_LOAD = list(map(int, getenv("NO_LOAD", "").split()))
